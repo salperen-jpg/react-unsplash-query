3a03646722247dce8c72a8768536b6dabda0c272
Salih Alperen <sal.alperen1@gmail.com>
2023-04-18 23:58:42 +0200

    react query initiliazation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,18 @@
 const Form = () => {
-  return <div>Form</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const val = e.target.elements.search.value;
+    if (!val) console.log('empty');
+    else {
+      console.log('smt');
+    }
+  };
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <input type='text' name='search' className='input' placeholder='dog' />
+      <button type='submit'>search</button>
+    </form>
+  );
 };
 export default Form;
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const response = await axios(
+        'https://api.unsplash.com/search/photos?page=1&query=dog&client_id=ihAgO_sB6bzHdaOJz2lueO-NYuIapOSWHohKDJV35Eo'
+      );
+      return response.data;
+    },
+  });
+
+  if (isLoading) {
+    return <div className='loading'></div>;
+  }
+
+  if (isError) {
+    return <div className='error'>There was an error...</div>;
+  }
+
+  return <section className='gallery'></section>;
 };
 export default Gallery;
